b2b4ff73156638bfdce5cdc6c874f77435df0df0
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2023-06-03 06:56:14 +0800

    added button

--- lib/screens/cart/cart.dart
+++ lib/screens/cart/cart.dart
@@ -36,58 +36,56 @@ class _ShoppingCartPageState extends State<ShoppingCartPage> {
         itemCount: widget.cartItems.length,
         itemBuilder: (context, index) {
           final item = widget.cartItems[index];
-          return Column(
-            children: [
-              ListTile(
-                title: Text("${item['name']}"),
-                subtitle: Text('${item['price']}'),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.add),
-                      onPressed: () {
-                        setState(() {
-                          item['quantity']++;
-                        });
-                        print(item['quantity']);
-                      },
-                    ),
-                    Text('${item['quantity']}'),
-                    IconButton(
-                      icon: Icon(Icons.remove),
-                      onPressed: () {
-                        setState(() {
-                          if (item['quantity'] > 1) item['quantity']--;
-                        });
-                        print(item['quantity']);
-                      },
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {
-                        setState(() {
-                          widget.cartItems.remove(item);
-                        });
-                      },
-                    ),
-                  ],
-                ),
-                /////
-              ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.red, // Set the desired color here
-                  ),
+          return ListTile(
+            title: Text("${item['name']}"),
+            subtitle: Text('${item['price']}'),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.add),
                   onPressed: () {
-                    //order placement functions
+                    setState(() {
+                      item['quantity']++;
+                    });
+                    print(item['quantity']);
                   },
-                  child: Text(
-                    "Proceed to Checkout",
-                  ))
-            ],
+                ),
+                Text('${item['quantity']}'),
+                IconButton(
+                  icon: Icon(Icons.remove),
+                  onPressed: () {
+                    setState(() {
+                      if (item['quantity'] > 1) item['quantity']--;
+                    });
+                    print(item['quantity']);
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {
+                    setState(() {
+                      widget.cartItems.remove(item);
+                    });
+                  },
+                ),
+              ],
+            ),
+            /////
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+              8.0), // Set the desired border radius for the rectangle
+        ),
+        child: Text(
+          "Checkout",
+          style: TextStyle(fontSize: 10.0),
+        ),
+        onPressed: () {},
       ),
     );
   }
