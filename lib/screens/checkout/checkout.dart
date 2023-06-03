bb5877a7cdbbfc8fca5d2934a4daf576a032b67e
Ishmum <ishmum20000@gmail.com>
2023-06-03 22:14:57 +0800

    Declare delivery details variables

--- lib/screens/checkout/checkout.dart
+++ lib/screens/checkout/checkout.dart
@@ -6,7 +6,33 @@ class CheckoutPage extends StatefulWidget {
   final double totalPrice;
 
   CheckoutPage({required this.totalPrice});
-  
+
   @override
-  State<CheckoutPage> createState() => _CheckoutPageState();
+  State<CheckoutPage> createState() => _CheckoutPage();
+}
+
+class _CheckoutPage extends State<CheckoutPage> {
+  String deliveryDate;
+  String deliveryTime;
+  String address;
+  String roomNumber;
+
+  List<String> deliveryDates = [
+    "Today",
+    "Next Day",
+    "Day After Tomorrow",
+  ];
+
+  List<String> deliveryTimes = [
+    "ASAP",
+    "After 30 mins",
+    "After 1 hour",
+  ];
+
+  List<String> addressOptions = [
+    "KLG Block A",
+    "KLG Block B",
+    "KLG Block C",
+    "KLG Block D",
+  ];
 }
