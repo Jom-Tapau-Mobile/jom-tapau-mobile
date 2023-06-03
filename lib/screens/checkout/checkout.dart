ca4ff219f68129d63beda6ef578872e4755ec0d8
Ishmum <ishmum20000@gmail.com>
2023-06-03 22:05:35 +0800

    Create CheckoutPage class

--- lib/screens/checkout/checkout.dart
+++ lib/screens/checkout/checkout.dart
@@ -1,2 +1,12 @@
 import 'package:flutter/material.dart';
 
+const DELIVERY_CHARGE = 1.00;
+
+class CheckoutPage extends StatefulWidget {
+  final double totalPrice;
+
+  CheckoutPage({required this.totalPrice});
+  
+  @override
+  State<CheckoutPage> createState() => _CheckoutPageState();
+}
